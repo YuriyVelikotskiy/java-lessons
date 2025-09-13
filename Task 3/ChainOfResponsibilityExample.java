28f7bdc341e0d3c07b22055d678f3a40882adfb1
Юрий <urabaket98@gmail.com>
2025-09-14 00:16:59 +0500

    Chain of responsibility pattern refactoring

--- Task 3/ChainOfResponsibilityExample.java
+++ Task 3/ChainOfResponsibilityExample.java
@@ -1,73 +1,89 @@
 public class ChainOfResponsibilityExample {
-        public abstract static class AuthenticationProcessor {
-            public AuthenticationProcessor nextProcessor;
 
-            protected String token;
-            protected String username;
-            protected String password;
 
-            public AuthenticationProcessor(String token, String username, String password) {
-                this.token = token;
-                this.username = username;
-                this.password = password;
-            }
+    public static class AuthenticationProvider {
+        protected String token;
+        protected String username;
+        protected String password;
 
-            public abstract boolean isAuthorized();
+
+        public AuthenticationProvider(String token, String username, String password) {
+            this.token = token;
+            this.username = username;
+            this.password = password;
         }
 
-        public static class TokenAuthenticationProcessor extends AuthenticationProcessor{
 
-            public TokenAuthenticationProcessor(String token, String username, String password){
-                super(token, username, password);
-            }
-
-            @Override
-            public boolean isAuthorized() {
-                if (token.equals("token")){
-                    System.out.println("Authentication completed (Token)");
-                    return true;
-                } else if (nextProcessor != null){
-                    return nextProcessor.isAuthorized();
-                }
-                System.out.println("Authentication failed");
-                return false;
-            }
+        public String getToken() {
+            return token;
         }
 
-        public static class PasswordAuthenticationProcessor extends AuthenticationProcessor{
-
-            public PasswordAuthenticationProcessor(String token, String username, String password){
-                super(token, username, password);
-            }
-
-            @Override
-            public boolean isAuthorized() {
-                if (password.equals("password") && username.equals("username")){
-                    System.out.println("Authentication completed (Username/Password)");
-                    return true;
-                } else if (nextProcessor != null){
-                    return nextProcessor.isAuthorized();
-                }
-                System.out.println("Authentication failed");
-                return false;
-            }
+        public String getUsername() {
+            return username;
         }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+
+    public abstract static class AuthenticationProcessor {
+        public AuthenticationProcessor nextProcessor;
+
+        public AuthenticationProcessor(AuthenticationProcessor next) {
+            this.nextProcessor = next;
+        }
+
+        public abstract boolean isAuthorized(AuthenticationProvider provider);
+    }
+
+    public static class TokenAuthenticationProcessor extends AuthenticationProcessor {
+
+        public TokenAuthenticationProcessor(AuthenticationProcessor next) {
+            super(next);
+        }
+
+        @Override
+        public boolean isAuthorized(AuthenticationProvider provider) {
+            if (provider.getToken().equals("token")) {
+                System.out.println("Authentication completed (Token)");
+                return true;
+            } else if (nextProcessor != null) {
+                return nextProcessor.isAuthorized(provider);
+            }
+            System.out.println("Authentication failed");
+            return false;
+        }
+    }
+
+    public static class PasswordAuthenticationProcessor extends AuthenticationProcessor {
+
+        public PasswordAuthenticationProcessor(AuthenticationProcessor next) {
+            super(next);
+        }
+
+        @Override
+        public boolean isAuthorized(AuthenticationProvider provider) {
+            if (provider.getPassword().equals("password") && provider.getUsername().equals("username")) {
+                System.out.println("Authentication completed (Username/Password)");
+                return true;
+            } else if (nextProcessor != null) {
+                return nextProcessor.isAuthorized(provider);
+            }
+            System.out.println("Authentication failed");
+            return false;
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.print("The correct token - ");
-        AuthenticationProcessor authenticationProcessor1 = new TokenAuthenticationProcessor("token", "asername", "password");
-        authenticationProcessor1.nextProcessor = new PasswordAuthenticationProcessor("token", "asername", "password");
-        authenticationProcessor1.isAuthorized();
+        AuthenticationProvider providerCorrectToken = new AuthenticationProvider("token", "asername", "password");
+        AuthenticationProvider providerUncorrectedToken = new AuthenticationProvider("taken", "username", "password");
+        AuthenticationProvider providerAllUncorrected = new AuthenticationProvider("taken", "asername", "password");
 
-        System.out.print("The correct username and password - ");
-        AuthenticationProcessor authenticationProcessor2 = new TokenAuthenticationProcessor("taken", "username", "password");
-        authenticationProcessor2.nextProcessor = new PasswordAuthenticationProcessor("taken", "username", "password");
-        authenticationProcessor2.isAuthorized();
-
-        System.out.print("All uncorrected - ");
-        AuthenticationProcessor authenticationProcessor3 = new TokenAuthenticationProcessor("taken", "asername", "password");
-        authenticationProcessor3.nextProcessor = new PasswordAuthenticationProcessor("taken", "asername", "password");
-        authenticationProcessor3.isAuthorized();
+        AuthenticationProcessor processor = new TokenAuthenticationProcessor(new PasswordAuthenticationProcessor(null));
+        processor.isAuthorized(providerCorrectToken);
+        processor.isAuthorized(providerUncorrectedToken);
+        processor.isAuthorized(providerAllUncorrected);
     }
 
 }
